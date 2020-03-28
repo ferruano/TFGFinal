628b295343adad69e783f43481e08fbfbc32066a
fernando <f.ruano@alumnos.upm.es>
2020-03-28 18:36:53 +0100

    cambio validacion de atributos

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ const config = {
 
     // This information is used to issue your "Government ID"
     userInformation: {
-        name: process.env.NAME || 'Alice Garcia',
+        nombre: process.env.NAME || 'Alice Garcia',
         email: process.env.EMAIL || 'alice@faber.edu',
         tax_id: process.env.TAX_ID || '123-45-6789',
         icon_src: process.env.ICON_SRC || 'https://cdn3.iconfinder.com/data/icons/black-easy/512/538738-school_512x512.png',
